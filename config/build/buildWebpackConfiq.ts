6bc4f9ee771afc34cf1a3cba0c3f2faba8df99c7
Anatoly <anatolysavchenko95@gmail.com>
2024-02-24 16:57:56 +0300

    5 Настраиваем css modules

--- config/build/buildWebpackConfiq.ts
+++ config/build/buildWebpackConfiq.ts
@@ -1,5 +1,5 @@
 import { BuildOptions } from "./types/config";
-import webpack from 'webpack';
+import webpack from "webpack";
 import path from "path";
 
 import { buildLoaders } from "./buildLoaders";
@@ -7,23 +7,25 @@ import { buildPlugins } from "./buildPlugins";
 import { buildResolvers } from "./buildResolvers";
 import { buildDevServer } from "./buildDevServer";
 
-export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
-    const {paths, mode, isDev} = options;
+export function buildWebpackConfig(
+  options: BuildOptions
+): webpack.Configuration {
+  const { paths, mode, isDev } = options;
 
-    return {
-        mode: mode,
-        entry: paths.entry,
-        output: {
-            filename: "[name].[contenthash].js",
-            path: paths.build,
-            clean: true
-        },
-        plugins: buildPlugins(options),
-        module: {
-            rules: buildLoaders(),
-        },
-        resolve: buildResolvers(),
-        devtool: isDev ? 'inline-source-map' : undefined,
-        devServer: isDev ? buildDevServer(options) : undefined
-    }
+  return {
+    mode: mode,
+    entry: paths.entry,
+    output: {
+      filename: "[name].[contenthash].js",
+      path: paths.build,
+      clean: true,
+    },
+    plugins: buildPlugins(options),
+    module: {
+      rules: buildLoaders(options),
+    },
+    resolve: buildResolvers(),
+    devtool: isDev ? "inline-source-map" : undefined,
+    devServer: isDev ? buildDevServer(options) : undefined,
+  };
 }
