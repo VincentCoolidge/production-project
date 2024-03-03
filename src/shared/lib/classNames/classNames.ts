d925eb3d1cd8d134c477af76e7bec89ebad3ecf2
Anatoly <anatolysavchenko95@gmail.com>
2024-03-03 21:34:49 +0300

    add Jest , first test

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,5 +1,3 @@
-import React from 'react';
-
 type Mods = Record<string, boolean | string>;
 
 export function classNames(
@@ -11,7 +9,7 @@ export function classNames(
         cls,
         ...additional.filter(Boolean),
         ...Object.entries(mods)
-            .filter(([className, value]) => Boolean(value))
+            .filter(([_, value]) => Boolean(value))
             .map(([className]) => className),
     ].join(' ');
 }
